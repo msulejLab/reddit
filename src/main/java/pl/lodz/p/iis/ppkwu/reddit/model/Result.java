9e8cea568ddf6a81016873d37949628a939be881
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-28 14:35:27 +0100

    equals, hashCode i toString dla Result

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/model/Result.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/model/Result.java
@@ -2,6 +2,7 @@ package pl.lodz.p.iis.ppkwu.reddit.model;
 
 import pl.lodz.p.iis.ppkwu.reddit.api.ResultStatus;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public class Result<R> implements pl.lodz.p.iis.ppkwu.reddit.api.Result<R> {
@@ -27,5 +28,27 @@ public class Result<R> implements pl.lodz.p.iis.ppkwu.reddit.api.Result<R> {
     @Override
     public Optional<R> content() {
         return Optional.ofNullable(content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Result<?> result = (Result<?>) o;
+        return resultStatus == result.resultStatus &&
+            Objects.equals(content, result.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(resultStatus, content);
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+            "resultStatus=" + resultStatus +
+            ", content=" + content +
+            '}';
     }
 }
