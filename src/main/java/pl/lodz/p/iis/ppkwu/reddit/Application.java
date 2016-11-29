5b3e70a931bf3f55071cf34818a2f292a729728a
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-29 12:19:48 +0100

    uzycie jraw

--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/Application.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/Application.java
@@ -1,8 +1,28 @@
 package pl.lodz.p.iis.ppkwu.reddit;
 
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.http.oauth.Credentials;
+import net.dean.jraw.http.oauth.OAuthData;
+import net.dean.jraw.http.oauth.OAuthException;
+import net.dean.jraw.models.Subreddit;
+import pl.lodz.p.iis.ppkwu.reddit.api.Reddit;
+import pl.lodz.p.iis.ppkwu.reddit.api.RedditBuilder;
+import pl.lodz.p.iis.ppkwu.reddit.api.Util;
+
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws OAuthException {
+/*
+        UserAgent myUserAgent = UserAgent.of("desktop", "pl.lodz.p.iis.ppkwu.reddit", "v1", "ppkwu-reddit");
+        RedditClient redditClient = new RedditClient(myUserAgent);
+        Credentials credentials = Credentials.script("ppkwu-reddit", "ppkwu-reddit", "XWHE1d691V9S4A", "r_5dy9HgYFYjG4aVIrgVenXZhCk");
 
+        OAuthData authData = redditClient.getOAuthHelper().easyAuth(credentials);
+        redditClient.authenticate(authData);
+*/
+
+        RedditBuilder redditBuilder = Util.redditBuilder();
+        Reddit reddit = redditBuilder.build();
     }
 }
--- src/main/java/pl/lodz/p/iis/ppkwu/reddit/impl/service/RedditService.java
+++ src/main/java/pl/lodz/p/iis/ppkwu/reddit/impl/service/RedditService.java
@@ -1,19 +1,36 @@
 package pl.lodz.p.iis.ppkwu.reddit.impl.service;
 
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.http.oauth.Credentials;
+import net.dean.jraw.http.oauth.OAuthData;
+import net.dean.jraw.http.oauth.OAuthException;
 import pl.lodz.p.iis.ppkwu.reddit.api.*;
 
 import java.util.List;
 import java.util.concurrent.Executor;
 
+import static java.lang.System.out;
+
 // TODO implement :-)
 public class RedditService implements Reddit {
 
     private Executor executor;
+    private final RedditClient redditClient;
 
-
-    // Tworzony przy pomocy buildera
     RedditService(Executor executor) {
         this.executor = executor;
+
+        UserAgent myUserAgent = UserAgent.of("desktop", "pl.lodz.p.iis.ppkwu.reddit", "v1", "ppkwu-reddit");
+        redditClient = new RedditClient(myUserAgent);
+        Credentials credentials = Credentials.script("ppkwu-reddit", "ppkwu-reddit", "XWHE1d691V9S4A", "r_5dy9HgYFYjG4aVIrgVenXZhCk");
+
+        OAuthData authData = null;
+        try {
+            authData = redditClient.getOAuthHelper().easyAuth(credentials);
+        } catch (OAuthException e) {
+            throw new IllegalArgumentException("Initialization exception", e);
+        }
     }
 
     @Override
